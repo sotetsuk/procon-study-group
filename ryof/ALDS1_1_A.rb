f47b87f5edfb946cda5565f2adf7a880d3b666f6
ryof <ryo.furuyama@gmail.com>
2016-04-08 15:47:09 +0900

    1_1_A accepted.

---
+++ ryof/ALDS1_1_A.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+n = STDIN.gets.chomp.to_i
+arr = STDIN.gets.chomp.split(' ').map {|e| e.to_i}
+puts arr.join(' ')
+for i in 1..n-1 do
+	v = arr[i]
+	j = i - 1
+	while j >= 0 && arr[j] > v do
+		arr[j+1] = arr[j]
+		j = j - 1
+	end
+	arr[j+1] = v
+	puts arr.join(' ')
+end
