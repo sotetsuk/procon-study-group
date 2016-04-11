73e2da0843e7f1910bb4878ffcc1e23aa393c578
ryof <ryo.furuyama@gmail.com>
2016-04-11 11:41:22 +0900

    scalaで書きなおした．2日目の問題を解いた

---
+++ sotetsuk/AOJ/src/ALDS1_2_A.scala
@@ -0,0 +1,26 @@
+import java.util.Scanner
+
+object ALDS1_2_A { // -> Main
+  def main(args: Array[String]) {
+    val sc = new Scanner(System.in)
+    val n = sc.nextInt
+    val list: Array[Int] = Array.fill(n)(sc.nextInt)
+
+    var flag = true
+    var count = 0
+    while (flag) {
+      flag = false
+      for (j <- n-1 to 1 by -1) {
+        if (list(j) < list(j - 1)) {
+          val tmp = list(j - 1)
+          list(j - 1) = list(j)
+          list(j) = tmp
+          count += 1
+          flag = true
+        }
+      }
+    }
+    println(list.toList.mkString(" "))
+    println(count)
+  }
+}
