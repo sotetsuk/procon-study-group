0913dba327a0abf13a3b08789f57d600cbcd4d06
樋口　彰 <a_higuchi@r.recruit.co.jp>
2016-05-23 20:47:30 +0900

    Dictionary(未衝突)

--- toiroakr/src/ALDS1_4_C.java
+++ toiroakr/src/ALDS1_4_C.java
@@ -14,18 +14,43 @@ public class ALDS1_4_C {
     }
 
     private void run() {
+        int[] dictionary = new int[16777216];
         int n = in.nextInt();
-        HashSet<String> dictionary = new HashSet<>();
         for (int i = 0; i < n; i++) {
             String order = in.next();
             String word = in.next();
+            int index = toInt(word);
             switch (order) {
                 case "insert":
-                    dictionary.add(word);
+                    dictionary[index]++;
                     break;
                 case "find":
-                    System.out.println(dictionary.contains(word) ? "yes" : "no");
+                    System.out.println(dictionary[index] > 0 ? "yes" : "no");
+                    break;
             }
         }
+    }
+
+    private int toInt(String word) {
+        int i = 0;
+        int mul = 1;
+        for (char charactor : word.toCharArray()) {
+            switch (charactor) {
+                case 'A':
+                    i += mul;
+                    break;
+                case 'C':
+                    i += mul * 2;
+                    break;
+                case 'T':
+                    i += mul * 3;
+                    break;
+                case 'G':
+                    i += mul * 4;
+                    break;
+            }
+            mul *= 5;
+        }
+        return i;
     }
 }
